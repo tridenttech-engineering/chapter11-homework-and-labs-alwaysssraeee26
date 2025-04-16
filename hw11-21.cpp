b3a2db9a133266ec4e74cf60535af860c6c068f4
alwaysssraeee26 <raechatt06@gmail.com>
2025-04-16 14:41:57 -0400

    Submission hw11-21.cpp

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,6 +1,6 @@
-// Lab11-2.cpp - stores stock prices in an array
+// Lab11-21.cpp - stores stock prices in an array
 // and displays the average price
-// Created/revised by <your name> on <current date>
+// Created/revised by <Your Name> on <Current Date>
 
 #include <iostream>
 #include <iomanip>
@@ -15,17 +15,17 @@ int main()
     double avgPrice = 0.0;
 
     // Enter stock prices
-    for (int x = 0; x < 5; x++)  // Using a for loop to enter 5 prices
+    for (int x = 0; x < 5; x++)  // Loop to enter 5 prices
     {
-        cout << "Price " << x + 1 << ": ";
-        cin >> prices[x];  // User enters each stock price
+        cout << "Enter price " << (x + 1) << ": ";
+        cin >> prices[x];
     }
 
     // Calculate average price using the getAverage function
     avgPrice = getAverage(prices, 5);
 
     // Display the average stock price with 2 decimal places
-    cout << fixed << setprecision(2);  // Ensure 2 decimal places
+    cout << fixed << setprecision(2);
     cout << "Average stock price: $" << avgPrice << endl;
 
     return 0;
@@ -36,12 +36,12 @@ double getAverage(double prices[], int numElements)
 {
     double total = 0.0;
 
-    // Sum all the stock prices using a for loop
+    // Sum all the stock prices
     for (int x = 0; x < numElements; x++)
     {
-        total += prices[x];  // Add each stock price to the total
+        total += prices[x];
     }
 
-    // Return the average price
+    // Return the average
     return total / numElements;
 } // end of getAverage function
